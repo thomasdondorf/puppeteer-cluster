e89e91a2a2ff2794d7c4d32780e227371afa56d2
Thomas Dondorf <thomasdondorf@gmail.com>
2018-06-16 20:56:14 +0200

    Alexa 1M example added

---
+++ examples/alexa-1m.js
@@ -0,0 +1,37 @@
+const Cluster = require('../lib/Cluster');
+
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+    const cluster = await Cluster.launch({
+        maxWorker: 2,
+        concurrency: Cluster.CONCURRENCY_BROWSER,
+    });
+
+    await cluster.setTask(async ({ url, page, cluster, context }) => {
+        await page.goto(url);
+        // ...
+    });
+
+    const csvFile = await readFile(__dirname + '/../data/top-1m.csv', 'utf8');
+    const lines = csvFile.split('\n');
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const splitterIndex = line.indexOf(',');
+        if (splitterIndex !== -1) {
+            const url = line.substr(splitterIndex + 1);
+            cluster.queue('http://www.' + url);
+        }
+    }
+
+    await cluster.idle();
+    await cluster.close();
+
+})();
