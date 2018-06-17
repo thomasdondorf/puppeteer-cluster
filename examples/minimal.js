002fc23fac90a60194a7fd0f11ac14de9ffb5f46
Thomas Dondorf <thomasdondorf@gmail.com>
2018-06-17 20:50:33 +0200

    Auto repair for context and page concurrency implemented

--- examples/minimal.js
+++ examples/minimal.js
@@ -7,22 +7,25 @@ function sleep(ms) {
 (async () => {
     const cluster = await Cluster.launch({
         maxWorker: 2,
-        concurrency: Cluster.CONCURRENCY_PAGE,
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
         // monitor: true,
     });
 
     await cluster.setTask(async ({ url, page, cluster, worker, context }) => {
         console.log('going to: ' + url);
-        await page.goto(url);
+        await page.goto(url, {
+            timeout: 5000,
+        });
+        console.log(' got there ' + url);
         // await sleep(3000);
         await page.screenshot({path: 'data/test123.png'});
-        console.log('DONE');
+        console.log('    DONE ' + url);
     });
 
     cluster.queue('http://www.google.com');
-    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
-    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
-    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
+    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md?1');
+    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md?22');
+    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md?333');
 
     await cluster.idle();
     await cluster.close();
