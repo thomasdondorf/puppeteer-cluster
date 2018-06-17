c1660d76ab4dfde2d04ef2a9d852977b3dbde9b4
Thomas Dondorf <thomasdondorf@gmail.com>
2018-06-17 15:00:46 +0200

    Basic error handling for the worker added

--- examples/minimal.js
+++ examples/minimal.js
@@ -1,5 +1,9 @@
 const Cluster = require('../lib/Cluster');
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 (async () => {
     const cluster = await Cluster.launch({
         maxWorker: 2,
@@ -9,9 +13,13 @@ const Cluster = require('../lib/Cluster');
 
     await cluster.setTask(async ({ url, page, cluster, worker, context }) => {
         await page.goto(url);
+        await sleep(5000);
         await page.screenshot({path: 'data/test123.png'});
     });
 
+    cluster.queue('http://asfdasfdla.casfdawerasdf.com');
+    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
+    cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
     cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
 
     await cluster.idle();
