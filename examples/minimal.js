9bc657539252031618d89d7424bb77691a2f37c2
Thomas Dondorf <thomasdondorf@gmail.com>
2018-06-17 14:06:29 +0200

    Worker ID added
    Makes ist easier to check which worker behaves weird

--- examples/minimal.js
+++ examples/minimal.js
@@ -2,23 +2,17 @@ const Cluster = require('../lib/Cluster');
 
 (async () => {
     const cluster = await Cluster.launch({
-        maxWorker: 4,
-        concurrency: Cluster.CONCURRENCY_PAGE,
+        maxWorker: 2,
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
         monitor: true,
     });
 
-    await cluster.setTask(async ({ url, page, cluster, context }) => {
-        // console.log('going to: ' + url);
-
+    await cluster.setTask(async ({ url, page, cluster, worker, context }) => {
         await page.goto(url);
         await page.screenshot({path: 'data/test123.png'});
     });
 
-    cluster.queue('https://www.google.com/');
-    cluster.queue('https://example.com');
     cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
-    cluster.queue('https://wikipedia.org');
-    cluster.queue('https://www.google.com/');
 
     await cluster.idle();
     await cluster.close();
