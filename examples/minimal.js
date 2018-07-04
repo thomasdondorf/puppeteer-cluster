13c2cca4e2f4dcbcc7e27364975cbca93094b558
Thomas Dondorf <thomasdondorf@gmail.com>
2018-07-04 20:18:25 +0200

    Minimal example improved

--- examples/minimal.js
+++ examples/minimal.js
@@ -16,9 +16,21 @@ const { Cluster } = require('../dist');
     await cluster.queue('http://www.google.com');
     await cluster.queue('http://www.wikipedia.org');
     await cluster.queue('https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md');
+
+    // You can also queue functions
     await cluster.queue(async (page) => {
-        // We can also queue functions
-        console.log('Just like this.');
+        await page.goto('https://www.npmjs.com/package/puppeteer-cluster');
+        // get the version from the npm, yes I know there is an API for that...
+        const version = await page.evaluate(() => {
+            const h3s = document.querySelectorAll('h3');
+            for (let i = 0; i < h3s.length; i += 1) {
+                if (h3s[i].innerText === 'version') {
+                    return h3s[i].nextSibling.innerText;
+                }
+            }
+            return 'Unknown';
+        })
+        console.log(`puppeteer-cluster is currently at version: ${version}`);
     });
 
     await cluster.idle();
