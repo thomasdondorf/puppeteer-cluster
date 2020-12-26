aeb992a8d0046dac6895c4f2fbc82c5ab58cec45
Daggie Blanqx (Douglas Mwangi) <daggieblanqx@gmail.com>
2020-12-26 15:09:32 +0300

    Updated the selectors.
    Google search results now have different selectors.

--- examples/deep-google-crawler.js
+++ examples/deep-google-crawler.js
@@ -25,17 +25,24 @@ const { Cluster } = require('../dist');
         console.log('Extracting Google results for offset=' + offset);
 
         // Extract the links and titles of the search result page
-        (await page.evaluate(() => {
-            return [...document.querySelectorAll('#ires .g .rc > .r a')]
-                .map(el => ({ url: el.href, name: el.innerText }));
-        })).forEach(({ url, name }, i) => {
-            // Put them into the cluster queue with the task "extractTitle"
-            console.log(`  Adding ${name} to queue`);
-            cluster.queue({
-                url,
-                position: (offset + i+1)
-            }, extractTitle);
-        });
+(await page.evaluate(() => {
+    return [...document.querySelectorAll('.yuRUbf')].map(rc => ({
+      url: rc.children[0].href,
+      name: rc.children[0].children[1].innerText,
+    }));
+  })).forEach(({ url, name }, i) => {
+    // Put them into the cluster queue with the task "extractTitle"
+    console.log(`  Adding ${name} to queue`);
+    cluster.queue(
+      {
+        url,
+        position: offset + i + 1,
+      },
+      extractTitle
+    );
+  });
+  
+        
     });
 
     cluster.queue({ searchTerm: 'puppeteer-cluster', offset: 0 });
