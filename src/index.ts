d60f2e1c365f60ba03dbc5174c83ee5d7e094c6e
Piotr Tarasiuk <piotr.tarasiuk@getresponse.com>
2025-05-29 07:11:19 +0200

    feat: Export certain types in order to create customised browser management

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 
 export { default as Cluster } from './Cluster';
+export { timeoutExecute, debugGenerator } from './util';
+export {
+    default as ConcurrencyImplementation,
+    type WorkerInstance,
+    type ResourceData,
+    type JobInstance,
+} from './concurrency/ConcurrencyImplementation';
